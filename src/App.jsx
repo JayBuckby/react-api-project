7c954b0fe621925b420efda6489738af86ebafe0
Jay <JJ.Buckby@hotmail.co.uk>
2022-12-07 15:38:47 +0000

    Linked JSX files with their SCSS counterparts

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import beers from "./assets/data/beers";
+
+const App = () => {
+  return (
+    <div className="display-page">
+      <header className="api-header">
+        <h1>PUNK API</h1>
+        <p>For all your drinking needs!</p>
+      </header>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default App;
